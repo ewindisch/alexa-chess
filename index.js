1374dba5c9831f19efa52886aa243f99e32e9005
Eric Windisch <eric@iopipe.com>
2016-05-29 02:06:55 -0400

    Two players
    Signed-off-by: Eric Windisch <eric@iopipe.com>

--- index.js
+++ index.js
@@ -11,35 +11,52 @@ exports.handler = function(event, context) {
   if (event.session.new) {
     response.response.outputSpeech = {
       type: "PlainText",
-      text: "Begin."
+      text: "Begin player 1."
     }
     response.sessionAttributes = {
       start: Date.now(),
-      timestamp: Date.now()
+      timestamp: Date.now(),
+      player: 0,
+      elapsed: [0, 0]
     }
   } else {
     response.sessionAttributes = event.session.attributes
     if (event.request.type === "IntentRequest") {
       var intent = event.request.intent.name
+      var player = parseInt(event.session.attributes.player)
+
       if (intent === "AMAZON.NextIntent") {
-        elapsed = Math.round((Date.now() - event.session.attributes.timestamp) / 1000)
+        elapsed = (Date.now() - event.session.attributes.timestamp) / 1000
+        response.sessionAttributes.player = (player)?0:1
         response.response.outputSpeech = {
           type: "PlainText",
-          text: elapsed + " seconds. Go."
+          text: Math.round(elapsed) + " seconds. Go player " + (response.sessionAttributes.player + 1)
         }
         response.sessionAttributes.timestamp = Date.now()
+        response.sessionAttributes.elapsed[player] = (event.session.attributes.elapsed[player] + elapsed)
       } else if (intent === "ElapsedIntent") {
         elapsed = Math.round((Date.now() - event.session.attributes.timestamp) / 1000)
         response.response.outputSpeech = {
           type: "PlainText",
-          text: elapsed + " seconds."
+          text: elapsed + " seconds for player " + (player + 1)
         }
         response.sessionAttributes.timestamp = response.session.attributes.timestamp
       } else if (intent === "AMAZON.StopIntent" || intent === "AMAZON.CancelIntent") {
-        elapsed = (Date.now() - event.session.attributes.start) / 1000
+        var endtime = Date.now()
+        elapsed = (endtime - event.session.attributes.start) / 1000
         minutes = Math.floor(elapsed / 60)
         secs = Math.round(elapsed % 60)
-        message = "Game lasted " + minutes + " minutes " + secs + " seconds."
+        p1elapsed = event.session.attributes.elapsed[0]
+        p1min = Math.floor(p1elapsed / 60)
+        p1sec = Math.round(p1elapsed % 60)
+        p2elapsed = event.session.attributes.elapsed[1]
+        p2min = Math.floor(p2elapsed / 60)
+        p2sec = Math.round(p2elapsed % 60)
+
+        gamemin = p1min + p2min
+        gamesec = p1sec + p2sec
+
+        message = "Game was played for " + minutes + " minutes " + secs + " seconds, for a total game time of " + gamemin + " minutes and " + gamesec + " seconds. Player one was clocked at " + p1min + " minutes " + p1sec + " seconds, while player two was clocked at " + p2min + " minutes " + p2sec + " seconds."
         response.response.outputSpeech = {
           type: "PlainText",
           text: message
